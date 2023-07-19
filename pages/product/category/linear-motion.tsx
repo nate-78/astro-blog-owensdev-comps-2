abeaceed430d0bec511ff8604f719743bbd709c3
nate-78 <nathan@owensdev.com>
2023-07-19 16:11:31 -0500

    adding linear motion page

---
+++ pages/product/category/linear-motion.tsx
@@ -0,0 +1,60 @@
+// pages/your-page.jsx
+
+import { BuilderComponent, builder } from '@builder.io/react';
+
+const BUILDER_API_KEY = 'de6d47b07dda425e8de672c214f22836';
+
+// Replace with your Public API Key.
+builder.init(BUILDER_API_KEY);
+
+export async function getStaticProps(props: any) {
+  // const urlPath = '/' + (props.params?.page?.join('/') || '');
+  const announce = await builder
+    .get('product-category-cta', {
+      query: {
+        data: {
+          categorySlug: '/product/category/linear-motion'
+        }
+      }
+    })
+    .toPromise();
+
+  return {
+    props: {
+      announce: announce || null,
+    },
+  };
+}
+
+// Define a function that generates the
+// static paths for all pages in Builder
+// export async function getStaticPaths() {
+//   // Get a list of all pages in Builder
+//   const pages = await builder.getAll("page", {
+//     // We only need the URL field
+//     fields: "data.url",
+//     options: { noTargeting: true },
+//   });
+
+//   // Generate the static paths for all pages in Builder
+//   return {
+//     paths: pages.map((page) => `${page.data?.url}`).filter(url => url !== '/'),
+//     fallback: 'blocking',
+//   };
+// }
+
+export default function Page(props: any) {
+  return (
+    <>
+      {/* Put your header here. */}
+      <div>
+        Other stuff goes here -- like built-in stuff
+      </div>
+      <BuilderComponent model="product-category-cta" content={props.announce} />
+      {/* Put the rest of your page here. */}
+      <div>
+        More stuff goes down here
+      </div>
+    </>
+  );
+}
