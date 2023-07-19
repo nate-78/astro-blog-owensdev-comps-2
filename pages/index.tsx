85d6fd79bcca2805b1444083e48b5f5c1101ab8c
nate-78 <nathan@owensdev.com>
2023-07-19 14:07:50 -0500

    pushing for another deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
               Docs <span>-&gt;</span>
             </h2>
             <p>
-              Find in-depth information about Next.js features and&nbsp;API.
+              Find in-depth information about Next.js features and&nbsp;API and stuff.
             </p>
           </a>
 
